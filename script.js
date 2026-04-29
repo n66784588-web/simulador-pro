611acd26479be3156f59b4e78e84e42bc4b0c5d2
n66784588-web <n66784588@gmail.com>
2026-04-29 11:08:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function cambiarAmbiente(tipo) {
   document.getElementById("ambiente").src =
     `img/habitaciones/${tipo}.jpg`;
 }
+const catalogo = document.getElementById("catalogo");
+
+productos.forEach(p => {
+  const img = document.createElement("img");
+
+  img.src = `img/pisos/${p.marca}/${p.nombre}.jpg`;
+  img.style.width = "120px";
+  img.style.margin = "10px";
+  img.style.cursor = "pointer";
+
+  img.onclick = () => {
+    document.getElementById("preview").src = img.src;
+    document.getElementById("nombre").innerText = p.nombre;
+  };
+
+  catalogo.appendChild(img);
+});

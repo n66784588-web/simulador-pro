1c0fd4065172e6ee9844b0b875f2d19b1271dfa5
n66784588-web <n66784588@gmail.com>
2026-04-29 12:23:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ function cambiarAmbiente(tipo) {
     `img/habitaciones/${tipo}.jpg`;
 }
 
-// CARGAR CATÁLOGO
+// CARGAR PRODUCTOS
 const catalogo = document.getElementById("catalogo");
 
 productos.forEach(p => {
   const img = document.createElement("img");
 
+  // 👇 ESTA ES LA CLAVE POR MARCA
   img.src = `img/pisos/${p.marca}/${p.nombre}.jpg`;
+
+  img.style.width = "120px";
+  img.style.margin = "10px";
+  img.style.cursor = "pointer";
 
   img.onclick = () => {
     document.getElementById("preview").src = img.src;
     document.getElementById("nombre").innerText = p.nombre;
-    document.getElementById("medida").innerText = "";
   };
 
   catalogo.appendChild(img);

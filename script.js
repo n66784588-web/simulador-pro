05250fb477362d893a0badf75a9fe365a8d88108
n66784588-web <n66784588@gmail.com>
2026-05-05 09:19:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,44 @@
-let productosFiltrados = [];
-let seleccionActual = "";
-
-/* CAMBIAR HABITACION */
 function cambiarAmbiente(tipo) {
   document.getElementById("ambiente").src =
     `img/habitaciones/${tipo}.jpg`;
 }
 
-/* FILTRAR POR MARCA */
-function filtrarMarca(marca) {
-  if (marca === "todas") {
-    productosFiltrados = productos;
-  } else {
-    productosFiltrados = productos.filter(p => p.marca === marca);
-  }
+function aplicarPiso(ruta, nombre) {
+  document.getElementById("piso").style.backgroundImage =
+    `url(${ruta})`;
 
-  mostrarCatalogo();
+  document.getElementById("nombre").innerText = nombre;
+  document.getElementById("preview").src = ruta;
 }
 
-/* MOSTRAR CATALOGO */
-function mostrarCatalogo() {
-  const catalogo = document.getElementById("catalogo");
-  catalogo.innerHTML = "";
+function cargarCatalogo(lista) {
+  const contenedor = document.getElementById("catalogo");
+  contenedor.innerHTML = "";
 
-  productosFiltrados.forEach(p => {
+  lista.forEach(p => {
     const div = document.createElement("div");
-    div.className = "item";
+    div.classList.add("item");
 
     div.innerHTML = `
       <img src="img/pisos/${p.marca}/${p.imagen}">
       <p>${p.nombre}</p>
     `;
 
-    div.onclick = () => aplicarPiso(p);
+    div.onclick = () => {
+      aplicarPiso(`img/pisos/${p.marca}/${p.imagen}`, p.nombre);
+    };
 
-    catalogo.appendChild(div);
+    contenedor.appendChild(div);
   });
 }
 
-/* APLICAR PISO */
-function aplicarPiso(p) {
-  seleccionActual = `img/pisos/${p.marca}/${p.imagen}`;
-
-  document.getElementById("piso").style.backgroundImage =
-    `url(${seleccionActual})`;
-
-  document.getElementById("nombre").innerText = p.nombre;
-  document.getElementById("preview").src = seleccionActual;
+function filtrarMarca(marca) {
+  if (marca === "todas") {
+    cargarCatalogo(productos);
+  } else {
+    const filtrados = productos.filter(p => p.marca === marca);
+    cargarCatalogo(filtrados);
+  }
 }
 
-/* INICIO */
-productosFiltrados = productos;
-mostrarCatalogo();
+cargarCatalogo(productos);
